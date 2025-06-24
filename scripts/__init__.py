94db8c004425a79492f64c5bdebc2cdc72597c13
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2025-06-24 11:59:59 -0400

    Refactor imports in __init__.py file

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,8 @@
 # scripts/__init__.py
 
-from .components.VideoArtifact import VideoArtifact, BatchArtifact, ArtifactFactory, BatchProcessor
+from .VideoArtifact import VideoArtifact, BatchArtifact, ArtifactFactory, BatchProcessor
+from .proxy         import MediaProxyArtifact
+
+# Add any other formerly-in-components here, e.g.:
+# from .VideoFacade  import VideoFacade
+# from .vtt          import VideoToText
